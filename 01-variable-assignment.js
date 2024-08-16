a92b30e7685e0bbf3ca0e3816920cd21a135e2c0
HarshadT04 <work.harshad04@gmail.com>
2024-08-16 13:14:44 +0530

    Javascript-variables
    Javascript-variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,9 @@
+console.log("My city name is: Pune");
+
+console.log("My best friend name is: Anand");
+
+console.log("My country name is: India");
+
+console.log("My school name is: Shri Siddheshwar Vidhyalaya");
+
+console.log("MY college Name is: GSMCOE, Pune");
